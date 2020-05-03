ace37bbb5f61d2ec9bc913b811f456548de5134a
krishanp-1986 <krishan1623@gmail.com>
2020-05-03 17:39:59 +0800

    Added Git Ignore

--- Movie Gene/AppDelegate.swift
+++ Movie Gene/AppDelegate.swift
@@ -7,7 +7,7 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    // Movie Gene
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
